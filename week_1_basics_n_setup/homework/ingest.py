35e1fd87a0a35290c4a8da3c6914c18fbb2a5747
Zhu Zhanyan <program.nom@gmail.com>
2022-07-06 14:14:05 +0000

    feat(week_1): ingest zone csv into NYTaxi database

--- week_1_basics_n_setup/homework/ingest.py
+++ week_1_basics_n_setup/homework/ingest.py
@@ -5,12 +5,16 @@
 #
 
 import os
+import logging as log
+from time import sleep
 import pandas as pd
 from pathlib import Path
 from argparse import ArgumentParser
 from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
 
 if __name__ == "__main__":
+    log.basicConfig(level=log.DEBUG)
     # parse pipeline arguments
     parser = ArgumentParser(
         description="Data Pipeline to ingest NYTaxi dataset into a Postgres DB",
@@ -35,10 +39,10 @@ the POSTGRES_PASSWORD environment variable.
         help="Size of each batch of Yellow Cab Taxi data imported into the Postgres DB",
     )
     parser.add_argument(
-        "--db-schema",
+        "--db-database",
         type=str,
-        default="ny_taxi",
-        help="Name of the Postgres DB schema to write tables to.",
+        default="NYTaxi",
+        help="Name of the Postgres DB database to write tables to.",
     )
     parser.add_argument(
         "--db-user", type=str, default="postgres",
@@ -47,11 +51,24 @@ the POSTGRES_PASSWORD environment variable.
     parser.parse_args()
     args = parser.parse_args()
 
-    # connect to the Postgres DB
+    # connect to the Postgres DB with retries
     db_password = os.environ["POSTGRES_PASSWORD"]
     db = create_engine(
-        f"postgresql+psycopg2://{args.db_user}:{db_password}@{args.db_host}/{args.db_schema}"
+        f"postgresql+psycopg2://{args.db_user}:{db_password}@{args.db_host}/{args.db_database}"
     )
+    for i in range(5):
+        try:
+            log.info(f"Connecting to DB: Retry {i+1}")
+            db.connect()
+            break
+        except OperationalError as e:
+            log.error(e)
+        sleep(1)
+    else:
+        raise RuntimeError("Failed to connect to database despite retries")
 
     # import taxi zone lookup data into Postgres
-    zone_df = pd.read_csv(args.zone_csv)
+    with db.begin():
+        zone_df = pd.read_csv(args.zone_csv)
+        zone_df.to_sql("PickupZone", db,
+                       if_exists="replace")
