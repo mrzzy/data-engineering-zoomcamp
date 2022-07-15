3ee7ec05d9c6c4ac298a962e18c587e0472458d2
Zhu Zhanyan <program.nom@gmail.com>
2022-07-15 08:59:24 +0000

    feat(week_1): indicate no. of chunks to ingest in pipeline logs

--- week_1_basics_n_setup/homework/ingest.py
+++ week_1_basics_n_setup/homework/ingest.py
@@ -16,6 +16,21 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 
+
+def count_lines(path: str) -> int:
+    """
+    Count the number of the lines in the text file of the given path.
+    """
+    buf_size = 1024 * 1024 # 1MB
+    n_lines = 0 
+    with open(path, "rb", buffering=0) as f:
+        buf = b"non empty"
+        while len(buf) > 0:
+            buf = f.read(buf_size)
+            n_lines += buf.count(b"\n")
+    return n_lines
+
+
 if __name__ == "__main__":
     log.basicConfig(level=log.DEBUG)
     # parse pipeline arguments
@@ -114,10 +129,14 @@ the POSTGRES_PASSWORD environment variable.
     zone_df.to_sql(zone_table, db, if_exists="append")
     log.info("Ingested Taxi Zone Lookup table to DB.")
 
+    # count taxi trip records needed to be ingested
+    n_trips = count_lines(args.cab_csv)
+    n_batches = n_trips // args.batch_size + 1
+
     # ingest taxi trip data in chunks
     for i, chunk_df in enumerate(trip_df):
         begin = time()
         chunk_df.to_sql(trip_table, db, if_exists="append")
         elapsed = timedelta(seconds=time() - begin)
-        log.info(f"Ingested Taxi Trips Chunk {i}: took {elapsed} ...")
+        log.info(f"Ingested Taxi Trips Chunk {i}/{n_batches}: took {elapsed} ...")
     log.info("Ingested Taxi Trips Data table to DB.")
