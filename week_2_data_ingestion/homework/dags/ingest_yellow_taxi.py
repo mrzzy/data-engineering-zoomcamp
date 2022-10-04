6ed781549bb5f06a5d8dcbd500ab9ccc2a4f123f
Zhu Zhanyan <program.nom@gmail.com>
2022-10-04 09:17:13 +0000

    feat(week_2): add dag to ingest for hire ny taxi dataset variant
    Squashed commit of the following:
    
    commit 4bd31f3909290fd9412948a255c1305f1c323e87
    Author: Zhu Zhanyan <program.nom@gmail.com>
    Date:   Tue Oct 4 09:16:01 2022 +0000
    
        refactor(week_2): lower dag runs per dag to 1 to avoid overwhelming system
    
    commit b8ee4f679075a7bc65fef3239f82b74fa53ca795
    Author: Zhu Zhanyan <program.nom@gmail.com>
    Date:   Tue Oct 4 09:15:28 2022 +0000
    
        feat(week_2): add dag to ingest for hire dataset
    
    commit baca0667aafd66b119da0ddc51e9cde4acbad969
    Author: Zhu Zhanyan <program.nom@gmail.com>
    Date:   Tue Oct 4 09:13:53 2022 +0000
    
        refactor(week_2): refactor build_dag() to parameterize ny taxi dataset variant

--- week_2_data_ingestion/homework/dags/ingest_yellow_taxi.py
+++ week_2_data_ingestion/homework/dags/ingest_yellow_taxi.py
@@ -4,6 +4,7 @@
 # Ingest Yellow Taxi Data to GCP
 #
 
+from enum import Enum
 import gzip, os
 from pendulum.tz.timezone import UTC
 import requests
@@ -14,130 +15,165 @@ from airflow.decorators import dag, task, task_group
 from pendulum import datetime
 from pendulum.datetime import DateTime
 from pyarrow import csv, parquet
+from airflow.models.dag import DAG
 from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.providers.google.cloud.operators.bigquery import (
-    BigQueryCreateEmptyDatasetOperator,
     BigQueryCreateExternalTableOperator,
     BigQueryDeleteTableOperator,
 )
 
 BUCKET = "dtc_data_lake_mrzzy-data-eng-zoomcamp"
-DATASET = "nyc_tlc"
+BQ_DATASET = "nyc_tlc"
 GCP_PROJECT = "mrzzy-data-eng-zoomcamp"
 
-@dag(
-    dag_id="ingest-yellow-taxi",
-    start_date=datetime(2019, 1, 2, tz=UTC),
-    end_date=datetime(2021, 8, 2, tz=UTC),
-    schedule_interval="0 3 2 * *",  # 3am on the 2nd of every month
-    catchup=True,
-    params={
-        "project_id": GCP_PROJECT,
-        "dataset": DATASET,
-        "retries": 3,
-        "retry_delay": 60.0,
-        "retry_exponential_backoff": True,
-    },
-)
-def build_dag():
+
+class NYTaxiDatasetType(Enum):
+    """NY Taxi Dataset variant types"""
+
+    Yellow = "yellow"
+    Green = "green"
+    ForHire = "fhv"
+
+
+def build_dag(
+    dataset_type: NYTaxiDatasetType,
+    gcp_project=GCP_PROJECT,
+    bucket=BUCKET,
+    bq_dataset=BQ_DATASET,
+) -> DAG:
     """
-    Ingest NY Taxi Yellow Cab Data into BigQuery.
-    Expects a Google Cloud Platform connection configured under the id:
-    `google_cloud_default` with GCS & BigQuery IAM permissions
+    Build an Airflow DAG to ingest the NYTaxi Dataset into BigQuery.
+
+    Args:
+        dataset_type:
+            Variant of the NYTaxi to ingest.
+        gcp_project:
+            GCP project containing the supporting infrastructure.
+        bucket:
+            Name of then GCS bucket to use as staging area for BigQuery ingestion.
+        bq_dataset
+            Name of the BigQuery dataset to ingest to.
     """
 
-    @task
-    def download(data_interval_start: Optional[DateTime] = None) -> str:
-        """
-        Download & Uncompress Yellow Cab Data CSV.
-        Returns the path to the downloaded CSV File.
-        """
-        # download gzipped data into buffer
-        partition = data_interval_start.strftime("%Y-%m")  # type: ignore
-        csv_path = f"yellow_tripdata_{partition}.csv"
-        with requests.get(
-            f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_{partition}.csv.gz"
-        ) as r, open(csv_path, "wb") as f:
-            csv = gzip.decompress(r.content)
-            f.write(csv)
-
-        return csv_path
-
-    @task
-    def convert_parquet(csv_path: str) -> str:
-        """
-        Converts the CSV file at the given file to Parquet.
-        Returns the path to the converted Parquet File.
-        """
-        # rewrite csv as parquet file
-        table = csv.read_csv(csv_path)
-        os.remove(csv_path)
-        pq_path = csv_path.replace("csv", "pq")
-        parquet.write_table(
-            table,
-            where=pq_path,
-            compression="snappy",
-        )
-        return pq_path
-
-    @task
-    def upload_gcs(src_path: str, bucket: str = BUCKET, prefix: str = DATASET) -> str:
-        """
-        Upload the file at the given path to the GCS with the given destination prefix.
-        Returns fully qualified GCS path to the file
-        """
-        gcs, dest_path = GCSHook(), f"{prefix}/{src_path}"
-        gcs.upload(
-            bucket_name=bucket,
-            object_name=dest_path,
-            filename=src_path,
-        )
-        os.remove(src_path)
-        return f"gs://{bucket}/{dest_path}"
-
-    @task_group
-    def register_bq_table(
-        gs_path: str,
-    ):
-        """
-        Register the given Parquet file on the GCS Bucket into BigQuery as a external table.
-        """
-        # fully replace existing table with new table if it already exists
-        table_name = "yellow_{{ data_interval_start.strftime('%Y_%m') }}"
-
-        remove_existing = BigQueryDeleteTableOperator(
-            task_id="remove_existing_table",
-            deletion_dataset_table="{{ params.dataset }}.%s" % table_name,
-            ignore_if_missing=True,
-        )
-
-        register_bq = BigQueryCreateExternalTableOperator(
-            task_id="ingest_parquet_bq_table",
-            table_resource={
-                "tableReference": {
-                    "datasetId": "{{ params.dataset }}",
-                    "tableId": table_name,
-                },
-                "externalDataConfiguration": {
-                    "sourceUris": [gs_path],
-                    "sourceFormat": "PARQUET",
-                },
-            }
-        )
-
-        remove_existing >> register_bq  # type: ignore
-
-    # define dag
-    csv_path = download()
-    pq_path = convert_parquet(csv_path)
-    gs_path = upload_gcs(pq_path, prefix=f"{DATASET}/raw")
-
-    register_bq = register_bq_table(gs_path)
-    create_dataset = BigQueryCreateEmptyDatasetOperator(
-        task_id="create_bq_dataset", dataset_id=DATASET, exists_ok=True,
-        location="europe-west6",
+    @dag(
+        dag_id=f"ingest-nyc-tlc-{dataset_type.value}",
+        start_date=datetime(2019, 1, 2, tz=UTC),
+        end_date=datetime(2021, 8, 2, tz=UTC),
+        schedule_interval="0 3 2 * *",  # 3am on the 2nd of every month
+        catchup=True,
+        # TODO(mrzzy): pushdown params to specific tasks
+        params={
+            "project_id": gcp_project,
+            "bq_dataset": bq_dataset,
+            "retries": 3,
+            "retry_delay": 60.0,
+            "retry_exponential_backoff": True,
+        },
     )
-    create_dataset >> register_bq # type: ignore
+    def build():
+        f"""
+        Ingest NY Taxi Data ({dataset_type.value}) into BigQuery.
+
+        ## Prerequisites
+        Expects a Google Cloud Platform connection configured under the id:
+        `google_cloud_default` with GCS & BigQuery IAM permissions.
+
+        Within the GCP project, expects the following infrastructure to be deployed:
+        - GCS Bucket: `{bucket}`
+        - BigQuery Dataset: `{bq_dataset}`
+        """
+
+        @task
+        def download(data_interval_start: Optional[DateTime] = None) -> str:
+            """
+            Download & Uncompress Data CSV.
+            Returns the path to the downloaded CSV File.
+            """
+            # download gzipped data into buffer
+            partition = data_interval_start.strftime("%Y-%m")  # type: ignore
+            csv_path = f"{dataset_type.value}_tripdata_{partition}.csv"
+            with requests.get(
+                f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{dataset_type.value}/{dataset_type.value}_tripdata_{partition}.csv.gz"
+            ) as r, open(csv_path, "wb") as f:
+                csv = gzip.decompress(r.content)
+                f.write(csv)
+
+            return csv_path
+
+        @task
+        def convert_parquet(csv_path: str) -> str:
+            """
+            Converts the CSV file at the given file to Parquet.
+            Returns the path to the converted Parquet File.
+            """
+            # rewrite csv as parquet file
+            table = csv.read_csv(csv_path)
+            os.remove(csv_path)
+            pq_path = csv_path.replace("csv", "pq")
+            parquet.write_table(
+                table,
+                where=pq_path,
+                compression="snappy",
+            )
+            return pq_path
+
+        @task
+        def upload_gcs(src_path: str, prefix: str, bucket: str = bucket) -> str:
+            """
+            Upload the file at the given path to the GCS with the given destination prefix.
+            Returns fully qualified GCS path to the file
+            """
+            gcs, dest_path = GCSHook(), f"{prefix}/{src_path}"
+            gcs.upload(
+                bucket_name=bucket,
+                object_name=dest_path,
+                filename=src_path,
+            )
+            os.remove(src_path)
+            return f"gs://{bucket}/{dest_path}"
+
+        @task_group
+        def register_bq_table(
+            gs_path: str,
+        ):
+            """
+            Register the given Parquet file on the GCS Bucket into BigQuery as a external table.
+            """
+            # fully replace existing table with new table if it already exists
+            table_name = (
+                dataset_type.value + "_{{ data_interval_start.strftime('%Y_%m') }}"
+            )
+
+            remove_existing = BigQueryDeleteTableOperator(
+                task_id="remove_existing_table",
+                deletion_dataset_table="{{ params.bq_dataset }}.%s" % table_name,
+                ignore_if_missing=True,
+            )
+
+            register_bq = BigQueryCreateExternalTableOperator(
+                task_id="ingest_parquet_bq_table",
+                table_resource={
+                    "tableReference": {
+                        "datasetId": "{{ params.bq_dataset }}",
+                        "tableId": table_name,
+                    },
+                    "externalDataConfiguration": {
+                        "sourceUris": [gs_path],
+                        "sourceFormat": "PARQUET",
+                    },
+                },
+            )
+
+            remove_existing >> register_bq  # type: ignore
+
+        # define dag
+        csv_path = download()
+        pq_path = convert_parquet(csv_path)
+        gs_path = upload_gcs(pq_path, prefix=f"nyc_tlc/{dataset_type.value}/raw")
+        register_bq_table(gs_path)
+
+    return build()
 
 
-dag = build_dag()
+yellow_dag = build_dag(NYTaxiDatasetType.Yellow)
+for_hire_dag = build_dag(NYTaxiDatasetType.ForHire)
