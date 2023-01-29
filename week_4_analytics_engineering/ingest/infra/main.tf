aa9daeb49de4cab11509c3184cde1693f77973eb
Zhu Zhanyan <program.nom@gmail.com>
2023-01-29 15:51:17 +0800

    feat(pip): add service to authenticate ingest pipeline

--- week_4_analytics_engineering/ingest/infra/main.tf
+++ week_4_analytics_engineering/ingest/infra/main.tf
@@ -1,12 +1,12 @@
 /*
  * Data Engineering Zoomcamp
  * Terraform Deployment
- * NYTaxi INgest Pipeline
+ * NYTaxi Ingest Pipeline
 */
 
 locals {
-  project_id =  "mrzzy-data-eng-zoomcamp"
-  region = "us-west1"
+  project_id = "mrzzy-data-eng-zoomcamp"
+  region     = "us-west1"
 }
 
 terraform {
@@ -34,12 +34,27 @@ provider "google" {
 
 # GCS bucket as data lake
 resource "google_storage_bucket" "lake" {
-  name = "${local.project_id}-nytaxi"
+  name     = "${local.project_id}-nytaxi"
   location = local.region
 }
 
 # BigQuery dataset as data warehouse
 resource "google_bigquery_dataset" "warehouse" {
   dataset_id = "nytaxi"
-  location = local.region
+  location   = local.region
+}
+
+# IAM: Ingest Pipeline Service Account
+resource "google_service_account" "pipeline" {
+  account_id  = "pipeline"
+  description = "Service Account to authenticate Ingest pipeline"
+}
+
+# IAM role bindings
+resource "google_project_iam_binding" "gcs" {
+  project = local.project_id
+  role = "roles/storage.objectAdmin"
+  members = [
+    google_service_account.pipeline.member
+  ]
 }
