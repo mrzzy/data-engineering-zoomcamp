74278d511e7f21515aee2c900b449d91d27c3e96
Zhu Zhanyan <program.nom@gmail.com>
2023-03-04 18:10:50 +0800

    feat(week6): prepare properties needed to config kafka client

--- week_6_stream_processing/java/kafka_follow_along/app/src/main/java/kafka_follow_along/JSONProducer.java
+++ week_6_stream_processing/java/kafka_follow_along/app/src/main/java/kafka_follow_along/JSONProducer.java
@@ -12,10 +12,38 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class JSONProducer {
-    public List<Ride> getRides() throws URISyntaxException {
+    private final Properties kafkaProperties;
+
+    JSONProducer() {
+        // load default properties from kafka properties file
+        kafkaProperties = new Properties();
+        try {
+            kafkaProperties.load(getClass().getResourceAsStream("kafka.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load defaults from kafka.properties: ", e);
+        }
+        // apply credentials from process environment into Properties
+        kafkaProperties.setProperty(
+                "sasl.jaas.config",
+                String.format(
+                        "org.apache.kafka.common.security.plain.PlainLoginModule"
+                                + " required username='%s' password='%s'",
+                        getEnv("CLUSTER_API_KEY"), getEnv("CLUSTER_API_SECRET")));
+    }
+
+    private String getEnv(String key) {
+        String value = System.getenv(key);
+        if (value == null) {
+            throw new RuntimeException(String.format("Expected environment variable: %s", key));
+        }
+        return value;
+    }
+
+    public List<Ride> getRides() {
         URL ridesURL = getClass().getResource("rides.csv");
         // rides url is null if getResource() is unable to find it
         if (ridesURL == null) {
@@ -30,10 +58,14 @@ public class JSONProducer {
                     .map(line -> Ride.parseTokens(line.split(",")))
                     .collect(Collectors.toList());
         } catch (URISyntaxException | IOException e) {
-            throw new RuntimeException(
-                    String.format("Unexpected exception reading rides.csv: %s\n", e));
+            throw new RuntimeException("Unexpected exception reading rides.csv:", e);
         }
     }
 
-    public static void main(String[] args) {}
+    public void publishRides(List<Ride> rides) {}
+
+    public static void main(String[] args) {
+        JSONProducer producer = new JSONProducer();
+        producer.getRides();
+    }
 }
