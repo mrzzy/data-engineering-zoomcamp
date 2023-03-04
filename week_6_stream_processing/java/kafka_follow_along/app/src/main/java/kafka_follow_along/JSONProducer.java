d301893b7cd448135819e773a1551c4db798e799
Zhu Zhanyan <program.nom@gmail.com>
2023-03-04 12:45:09 +0800

    chore(week6): setup gradle java app template to follow along 6.5 kafka producer consumer

---
+++ week_6_stream_processing/java/kafka_follow_along/app/src/main/java/kafka_follow_along/JSONProducer.java
@@ -0,0 +1,12 @@
+/*
+ * data-engineering-zoomcamp
+ * Week 6
+ * Kafka Follow Along
+ */
+
+package kafka_follow_along;
+
+public class JSONProducer {
+    public static void main(String[] args) {
+    }
+}
