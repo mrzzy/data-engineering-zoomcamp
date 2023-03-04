ab2a2ee3203cf4358819f97b4a97a23f1813011d
Zhu Zhanyan <program.nom@gmail.com>
2023-03-04 17:31:41 +0800

    fix(week_6): testGetRides assertions not matching up with actual data

--- week_6_stream_processing/java/kafka_follow_along/app/src/test/java/kafka_follow_along/JSONProducerTest.java
+++ week_6_stream_processing/java/kafka_follow_along/app/src/test/java/kafka_follow_along/JSONProducerTest.java
@@ -21,21 +21,21 @@ public class JSONProducerTest {
         assertEquals(rides.get(0).vendorID(), 1);
         assertEquals(rides.get(0).tpepPickupDatetime(), LocalDateTime.parse("2020-07-01T00:25:32"));
         assertEquals(
-                rides.get(0).tpepDropoffDatetime(), LocalDateTime.parse("2020-07-01T00:38:57"));
+                rides.get(0).tpepDropoffDatetime(), LocalDateTime.parse("2020-07-01T00:33:39"));
         assertEquals(rides.get(0).passengerCount(), 1);
-        assertEquals(rides.get(0).tripDistance(), 14.2, 1e-15);
+        assertEquals(rides.get(0).tripDistance(), 1.5, 1e-15);
         assertEquals(rides.get(0).RatecodeID(), 1);
         assertEquals(rides.get(0).storeAndFwdFlag(), "N");
-        assertEquals(rides.get(0).PULocationID(), 75);
-        assertEquals(rides.get(0).DOLocationID(), 236);
-        assertEquals(rides.get(0).payment_type(), 1);
-        assertEquals(rides.get(0).fareAmount(), 7.0, 1e-15);
+        assertEquals(rides.get(0).PULocationID(), 238);
+        assertEquals(rides.get(0).DOLocationID(), 75);
+        assertEquals(rides.get(0).payment_type(), 2);
+        assertEquals(rides.get(0).fareAmount(), 8.0, 1e-15);
         assertEquals(rides.get(0).extra(), 0.5, 1e-15);
         assertEquals(rides.get(0).mtaTax(), 0.5, 1e-15);
-        assertEquals(rides.get(0).tipAmount(), 2.7, 1e-15);
+        assertEquals(rides.get(0).tipAmount(), 0.0, 1e-15);
         assertEquals(rides.get(0).tollsAmount(), 0.0, 1e-15);
         assertEquals(rides.get(0).improvementSurcharge(), 0.3, 1e-15);
-        assertEquals(rides.get(0).totalAmount(), 13.5, 1e-15);
-        assertEquals(rides.get(0).congestionSurcharge(), 2.5, 1e-15);
+        assertEquals(rides.get(0).totalAmount(), 9.3, 1e-15);
+        assertEquals(rides.get(0).congestionSurcharge(), 0.0, 1e-15);
     }
 }
