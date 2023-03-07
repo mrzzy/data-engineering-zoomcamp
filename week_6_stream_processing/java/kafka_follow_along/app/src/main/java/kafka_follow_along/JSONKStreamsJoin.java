f1669d116a4031d51165dc51716a47fca4babfa5
Zhu Zhanyan <program.nom@gmail.com>
2023-03-07 08:03:42 +0000

    feat(week6): setup kafka stream application boilerplate

---
+++ week_6_stream_processing/java/kafka_follow_along/app/src/main/java/kafka_follow_along/JSONKStreamsJoin.java
@@ -0,0 +1,36 @@
+/*
+ * data-engineering-zoomcamp
+ * Week 6
+ * Kafka Follow Along - JSONConsumer
+ */
+
+package kafka_follow_along;
+
+import java.util.Properties;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.Topology;
+
+public class JSONKStreamsJoin {
+    public Topology build(String inRidesTopic, String inLocationsTopic, String outTopic) {
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        // build kafka streams application
+        JSONKStreamsJoin join = new JSONKStreamsJoin();
+        Properties properties = KafkaProperties.load();
+        KafkaStreams kafkaStreams = new KafkaStreams(join.build(
+                properties.getProperty("dezoomcamp.kafka.join.rides-topic"),
+                properties.getProperty("dezoomcamp.kafka.join.location-topic"),
+                properties.getProperty("dezoomcamp.kafka.join.out-topic")),
+                properties);
+        kafkaStreams.start();
+
+        // wait for application to start up
+        while (kafkaStreams.state() != KafkaStreams.State.RUNNING) {
+            System.out.println(String.format("Starting: %s", kafkaStreams.state()));
+            Thread.sleep(1000);
+        }
+        System.out.println("Started: Stream Live!");
+    }
+}
